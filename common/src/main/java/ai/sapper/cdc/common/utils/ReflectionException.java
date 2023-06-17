26d8dac486c3f0d3eceab0b7c4d02ee614acfc1b
Subho Ghosh <subho.ghosh@outlook.com>
2023-06-17 16:27:32 +0530

    Project Refactoring

--- common/src/main/java/ai/sapper/cdc/common/utils/ReflectionException.java
+++ common/src/main/java/ai/sapper/cdc/common/utils/ReflectionException.java
@@ -1,29 +1,20 @@
-package ai.sapper.cdc.common.utils;
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright(C) (2023) Sapper Inc. (open.source at zyient dot io)
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Copyright (c) $year
- * Date: 9/2/19 11:12 PM
- * Subho Ghosh (subho dot ghosh at outlook.com)
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-
+package ai.sapper.cdc.common.utils;
 /**
  * Exception instance used to raise error for state checks.
  */
