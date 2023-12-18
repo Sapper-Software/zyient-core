ece412b940444e80962dcf240ccf7a85d5558c7b
Subho Ghosh <subho.ghosh@outlook.com>
2023-12-18 17:31:50 +0530

    Refactoring Changes

--- mapping/src/main/java/io/zyient/core/mapping/config/RegexGroupParser.java
+++ mapping/src/main/java/io/zyient/core/mapping/config/RegexGroupParser.java
@@ -4,16 +4,18 @@ import com.google.common.base.Strings;
 import io.zyient.base.common.config.ConfigValueParser;
 import lombok.NonNull;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class RegexGroupParser implements ConfigValueParser<Map<Integer, Integer>> {
+public class RegexGroupParser implements ConfigValueParser<Map<Integer, List<Integer>>> {
     @Override
-    public Map<Integer, Integer> parse(@NonNull String value) throws Exception {
+    public Map<Integer, List<Integer>> parse(@NonNull String value) throws Exception {
         if (!Strings.isNullOrEmpty(value)) {
             String[] parts = value.split(",");
             if (parts.length > 0) {
-                Map<Integer, Integer> values = new HashMap<>();
+                Map<Integer, List<Integer>> values = new HashMap<>();
                 for (String part : parts) {
                     String[] vs = part.split(":");
                     if (vs.length != 2) {
@@ -21,7 +23,14 @@ public class RegexGroupParser implements ConfigValueParser<Map<Integer, Integer>
                     }
                     int m = Integer.parseInt(vs[0]);
                     int g = Integer.parseInt(vs[1]);
-                    values.put(m, g);
+                    List<Integer> list = null;
+                    if (values.containsKey(m)) {
+                        list = values.get(m);
+                    } else {
+                        list = new ArrayList<>();
+                        values.put(m, list);
+                    }
+                    list.add(g);
                 }
                 return values;
             }
@@ -30,7 +39,7 @@ public class RegexGroupParser implements ConfigValueParser<Map<Integer, Integer>
     }
 
     @Override
-    public String serialize(@NonNull Map<Integer, Integer> value) throws Exception {
+    public String serialize(@NonNull Map<Integer, List<Integer>> value) throws Exception {
         return null;
     }
 }
