db96bcb724349f247c2d30ee23e4a31918057967
Vinay Kumar <vinay.kumar@zyient.io>
2024-02-01 00:29:39 +0530

    fix: mapping wildcard

---
+++ mapping/src/main/java/io/zyient/core/mapping/model/mapping/WildcardMappedElement.java
@@ -0,0 +1,12 @@
+package io.zyient.core.mapping.model.mapping;
+
+import io.zyient.base.common.config.Config;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WildcardMappedElement extends  MappedElement{
+    @Config(name = "prefix")
+    private String prefix;
+}
