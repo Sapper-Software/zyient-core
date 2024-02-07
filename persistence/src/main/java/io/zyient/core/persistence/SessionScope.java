71195e611b93e97c707283fa49d76df20d09d7d1
Subho Ghosh <subho.ghosh@outlook.com>
2024-02-07 16:08:47 +0530

    Project Refactoring

---
+++ persistence/src/main/java/io/zyient/core/persistence/SessionScope.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright(C) (2024) Zyient Inc. (open.source at zyient dot io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.zyient.core.persistence;
+
+import lombok.NonNull;
+
+public interface SessionScope {
+    enum With {
+        ReadOnly,
+        Commit,
+        Rollback
+    }
+
+    void beingSession(boolean readOnly) throws DataStoreException;
+
+    void endSession(@NonNull With with) throws DataStoreException;
+
+    <T> T endSession(@NonNull With with, T ret) throws DataStoreException;
+
+    void endIfOpen() throws DataStoreException;
+}
